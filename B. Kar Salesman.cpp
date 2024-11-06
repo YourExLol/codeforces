3f7e1b928af8e75b6e801da06ff06801045e70c3
YourExLol <71457329+YourExLol@users.noreply.github.com>
2024-11-06 13:51:34 +0530

    Create B. Kar Salesman.cpp

---
+++ B. Kar Salesman.cpp
@@ -0,0 +1,5 @@
+## Problem Statement ##
+## B. Kar Salesman
+## https://codeforces.com/problemset/problem/2022/B
+
+############################################### Code begins here ##############################################################
